bae1af3b1cc6175d43d70a4775d557a83b06d393
mhlam <milogammond@gmail.com>
2023-11-01 17:33:18 +0000

    removed icons

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,8 @@
-import icon from "../assets/earth-icon.png";
-
 const Navbar = () => {
   return (
     <>
       <div className="flex justify-center items-center px-20 py-5 nav-bar">
-        <img src={icon} width="30px" />
         <h1 className="font-bold text-2xl mx-3">Flag Helper</h1>
-        <img src={icon} width="30px" />
       </div>
     </>
   );
