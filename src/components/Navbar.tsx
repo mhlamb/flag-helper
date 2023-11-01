fc893f3ee9fc9ee3c4c1ab50b3f5787a79c75e30
mhlam <milogammond@gmail.com>
2023-11-01 17:18:51 +0000

    updated icon img tags

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,9 +2,9 @@ const Navbar = () => {
   return (
     <>
       <div className="flex justify-center items-center px-20 py-5 nav-bar">
-        <img src="../public/earth-icon.png" width="30px" />
+        <img src="./earth-icon.png" width="30px" />
         <h1 className="font-bold text-2xl mx-3">Flag Helper</h1>
-        <img src="../public/earth-icon.png" width="30px" />
+        <img src="./earth-icon.png" width="30px" />
       </div>
     </>
   );
