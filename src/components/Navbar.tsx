243359a372150f562c2ddc96ba3527c41a010432
mhlam <milogammond@gmail.com>
2023-11-01 17:26:20 +0000

    updated img tag

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import icon from "../assets/earth-icon.png";
+
 const Navbar = () => {
   return (
     <>
       <div className="flex justify-center items-center px-20 py-5 nav-bar">
-        <img src="./earth-icon.png" width="30px" />
+        <img src={icon} width="30px" />
         <h1 className="font-bold text-2xl mx-3">Flag Helper</h1>
-        <img src="./earth-icon.png" width="30px" />
+        <img src={icon} width="30px" />
       </div>
     </>
   );
